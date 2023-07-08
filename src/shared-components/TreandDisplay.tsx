445ceea2134c16ffadc01bf02223f115dd9b65ae
adeyemi ademola <ademolaadeyemieric@gmail.com>
2023-07-08 13:17:40 +0100

    the price toggle completed completed

--- src/shared-components/TreandDisplay.tsx
+++ src/shared-components/TreandDisplay.tsx
@@ -1,6 +1,13 @@
+import {  MdDone } from 'react-icons/md';
+
 const TreandDisplay = () => {
 
   const toggleBox = {
+    margin: "30px 0",
+    alignItems: "center",
+  }
+
+  const toggleBoxContent = {
     display: "flex",
     margin: "auto",
     width: "622.5px",
@@ -8,25 +15,60 @@ const TreandDisplay = () => {
     border: "1px solid #ddd",
     borderRadius: "15px",
     height: "66px",
-    // cursor: "pointer",
-    // justifyContent: "right",
-    // boxShadow: "0 1.5px 5px #dddddd",
-    backgroundColor: "red"
+    justifyContent: "space-between",
+    // backgroundColor: "red"
+  }
+
+  const toggleBoxText = {
+    padding: "20px",
+    display: "flex",
+  }
+
+  const toggleBoxToggle ={
+    marginRight: "20px",
+    width: "50px",
+    backgroundColor: "#b0b0b0",
+    borderRadius: "55px",
+    border: "1px solid #ddd",
+    padding: "2px 2px", 
+  }
+
+  const button ={
+    height: "30px",
+    width: "30px",
+    borderRadius: "66px",
+    border: "none",
   }
 
   return (
-    <div>
       
-      <div style={toggleBox}>
-        <div>
-          <div></div>
-          <button></button>
+    <div style={toggleBox}>
+
+      <div style={toggleBoxContent}>
+        <div style={toggleBoxText}>
+          <p>
+            Display total price
+          </p>
+
+          <div style={
+            {height: "25px", 
+            width: "1.2px", 
+            backgroundColor: "#dddddd",
+            margin: "0 15px"}
+            }>
+          </div>
+
+          <p>
+            Includes all fees, before taxes
+          </p>
         </div>
-        exdcrfvygbuhnjkml,
+        <div style={toggleBoxToggle}>
+          <button style={button}><MdDone/></button>
+        </div>
       </div>
 
     </div>
+
   )
 }
 export default TreandDisplay
-
